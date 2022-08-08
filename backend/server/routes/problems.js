38cdb2c93b151d255edc6afab331374efae513e5
todorokishotoua15 <todorokishoto.ua15@gmail.com>
2022-08-09 00:26:51 +0530

    Completed Pending Problem Implementation

--- backend/server/routes/problems.js
+++ backend/server/routes/problems.js
@@ -6,17 +6,50 @@ User = require('../models/user');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
 
-router.get('/', authenticate.verifyUser, function(req,res,next) {
+const problemrouter = express.Router();
+problemrouter.use(bodyParser.json());
+
+problemrouter.route('/')
+.get(authenticate.verifyUser, function(req,res,next) {
     var username = req.headers.username;
     User.findOne({username: username}).then((user) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
+        console.log(user);
         res.json(user.problems);
     }, (err) => next(err))
     .catch((err) => {
-        next(err);
+        console.log(err);
+    })
+});
+problemrouter.route('/upd')
+.post((req,res) => {
+
+    User.findOne({username: req.body.username})
+    .then((user) => {
+        
+        var temp = user.problems;
+        for (var i = 0; i < req.body.problems.length; i++) {
+            var found = false;
+            var curr = req.body.problems[i];
+            for (var j = 0; j < temp; j++) {
+                if (temp[j].rating === curr.rating && temp[j].name === curr.name && temp[j].contest === curr.contest){
+                    found = true;
+                }
+            }
+            if (!found) temp.push(req.body.problems[i]);
+        }
+        user.problems = temp;
+        console.log(temp);
+        user.save().then((user) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            console.log(user.problems);
+            res.send(user);
+        })
     })
 })
 
-module.exports = router;
+module.exports = problemrouter;
